fd758d1c7baa03768b1424b5de2e98458d4c61ce
VChet <17050347+VChet@users.noreply.github.com>
2019-01-21 23:37:51 +0500

    Refactor and tweaks
    Temporarily store db in the script file
    Store timecode in dataset
    Add sendTiming function
    Add error checkers

--- rewind.user.js
+++ rewind.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube Ads Rewind
-// @version      0.1
+// @version      0.1.0
 // @description  Rewinds embedded advertisements
 // @author       VChet
 // @supportURL   https://github.com/VChet/Youtube-Ads-Rewind/issues
@@ -8,8 +8,8 @@
 // @match        https://www.youtube.com/*
 // ==/UserScript==
 
-(function () {
-  'use strict'
+(async () => {
+  "use strict"
 
   const ytMenu = document.querySelector("#watch8-secondary-actions");
   const ytPlayer = document.getElementById("movie_player");
@@ -17,26 +17,46 @@
   const stopButton = document.createElement("button");
   const sendButton = document.createElement("button");
 
-  startButton.addEventListener("click", setTiming);
-  stopButton.addEventListener("click", setTiming);
-  //sendButton.addEventListener("click", sendTiming);
-
   function setTiming() {
-    function secondsToHms(time) {
+    const secondsToHms = time => {
       time = Number(time);
       const h = Math.floor(time / 3600);
       const m = Math.floor(time % 3600 / 60);
       const s = Math.floor(time % 3600 % 60);
-      if (h > 0) {
-        return `${h}:${m}:${s < 10 ? "0" + s : s}`;
-      } else {
-        return `${m}:${s < 10 ? "0" + s : s}`;
-      }
+      return h > 0 ? `${h}:${m}:${s < 10 ? "0" + s : s}` : `${m}:${s < 10 ? "0" + s : s}`;
     }
-    this.querySelector("span").textContent = `${secondsToHms(ytPlayer.getCurrentTime())}`;
+    this.querySelector("span").dataset.timecode = Math.trunc(ytPlayer.getCurrentTime());
+    return this.querySelector("span").textContent = secondsToHms(ytPlayer.getCurrentTime());;
   }
 
-  const styleClasses = ["yt-uix-button", "yt-uix-button-content", "yt-uix-button-size-default", "yt-uix-button-opacity", "no-icon-markup", "pause-resume-autoplay", "action-panel-trigger", "yt-uix-tooltip"];
+  function sendTiming() {
+    const starts = startButton.querySelector("span").dataset.timecode;
+    const ends = stopButton.querySelector("span").dataset.timecode;
+    if (starts && ends) {
+      if (starts > ends) {
+        return console.warn("[YouTube Ads Rewind] Incorrect time codes");
+      }
+      const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
+      return console.log({ videoId, starts, ends });
+    } else {
+      return console.warn("[YouTube Ads Rewind] No time codes");
+    }
+  }
+
+  startButton.addEventListener("click", setTiming);
+  stopButton.addEventListener("click", setTiming);
+  sendButton.addEventListener("click", sendTiming);
+
+  const styleClasses = [
+    "action-panel-trigger",
+    "no-icon-markup",
+    "pause-resume-autoplay",
+    "yt-uix-button-content",
+    "yt-uix-button-opacity",
+    "yt-uix-button-size-default",
+    "yt-uix-button",
+    "yt-uix-tooltip"
+  ];
 
   startButton.classList.add(...styleClasses);
   stopButton.classList.add(...styleClasses);
@@ -48,33 +68,11 @@
 
   ytMenu.append(startButton, stopButton, sendButton);
 
-  const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
-  console.log(`[YouTube Ads Rewind] ${videoId}`);
-  checkDB();
-
-  async function checkDB() {
-    let videosList = await makeRequest("GET", "https://raw.githubusercontent.com/VChet/Youtube-Ads-Rewind/master/db.json");
-    videosList = JSON.parse(videosList);
-    if (Object.keys(videosList).indexOf(videoId) > -1) {
-      console.log(`[YouTube Ads Rewind] Rewind from ${videosList[videoId].start} to ${videosList[videoId].stop}`);
-      setInterval(function () {
-        if (ytPlayer.getPlayerState() == 1) {
-          if (ytPlayer.getCurrentTime().toFixed() == videosList[videoId].start) {
-            console.log("[YouTube Ads Rewind] Rewinding...");
-            ytPlayer.seekTo(videosList[videoId].stop);
-          }
-        }
-      }, 100);
-    } else {
-      return;
-    }
-  }
-
-  function makeRequest(method, url) {
-    return new Promise(function (resolve, reject) {
+  const makeRequest = (method, url) => {
+    return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.open(method, url);
-      xhr.onload = function () {
+      xhr.onload = () => {
         if (this.status === 200) {
           resolve(xhr.response);
         } else {
@@ -84,7 +82,7 @@
           });
         }
       };
-      xhr.onerror = function () {
+      xhr.onerror = () => {
         reject({
           status: this.status,
           statusText: xhr.statusText
@@ -93,4 +91,34 @@
       xhr.send();
     });
   }
+
+  function rewind() {
+    if (Object.keys(videosList).indexOf(videoId) > -1) {
+      console.log(`[YouTube Ads Rewind] Rewind from ${videosList[videoId].starts} to ${videosList[videoId].ends}`);
+      setInterval(() => {
+        if (ytPlayer.getPlayerState() == 1) {
+          if (ytPlayer.getCurrentTime().toFixed() == videosList[videoId].starts) {
+            console.log("[YouTube Ads Rewind] Rewinding...");
+            ytPlayer.seekTo(videosList[videoId].ends);
+          }
+        }
+      }, 100);
+    } else {
+      return console.warn("[YouTube Ads Rewind] Database is empty");
+    }
+  }
+
+  const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
+  console.log(`[YouTube Ads Rewind] Current video ID: ${videoId}`);
+
+  // let videosList = await makeRequest("GET", "https://raw.githubusercontent.com/VChet/Youtube-Ads-Rewind/master/db.json");
+  let videosList = {
+    "zSakqtywY5c": {
+      "starts": "4",
+      "ends": "54"
+    }
+  }
+  // videosList = JSON.parse(videosList);
+  console.log("[YouTube Ads Rewind] DB has been loaded");
+  rewind();
 })();
