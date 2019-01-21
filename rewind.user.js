e676cc5e98a7db9125e7335e29645658090e9a9d
VChet <17050347+VChet@users.noreply.github.com>
2019-01-21 21:54:59 +0500

    Fix indentation

--- rewind.user.js
+++ rewind.user.js
@@ -9,88 +9,88 @@
 // ==/UserScript==
 
 (function () {
-    'use strict'
+  'use strict'
 
-    const ytMenu = document.querySelector("#watch8-secondary-actions");
-    const ytPlayer = document.getElementById("movie_player");
-    const startButton = document.createElement("button");
-    const stopButton = document.createElement("button");
-    const sendButton = document.createElement("button");
+  const ytMenu = document.querySelector("#watch8-secondary-actions");
+  const ytPlayer = document.getElementById("movie_player");
+  const startButton = document.createElement("button");
+  const stopButton = document.createElement("button");
+  const sendButton = document.createElement("button");
 
-    startButton.addEventListener("click", setTiming);
-    stopButton.addEventListener("click", setTiming);
-    //sendButton.addEventListener("click", sendTiming);
+  startButton.addEventListener("click", setTiming);
+  stopButton.addEventListener("click", setTiming);
+  //sendButton.addEventListener("click", sendTiming);
 
-    function setTiming() {
-        function secondsToHms(time) {
-            time = Number(time);
-            const h = Math.floor(time / 3600);
-            const m = Math.floor(time % 3600 / 60);
-            const s = Math.floor(time % 3600 % 60);
-            if (h > 0) {
-                return `${h}:${m}:${s < 10 ? "0" + s : s}`;
-            } else {
-                return `${m}:${s < 10 ? "0" + s : s}`;
-            }
-        }
-        this.querySelector("span").textContent = `${secondsToHms(ytPlayer.getCurrentTime())}`;
+  function setTiming() {
+    function secondsToHms(time) {
+      time = Number(time);
+      const h = Math.floor(time / 3600);
+      const m = Math.floor(time % 3600 / 60);
+      const s = Math.floor(time % 3600 % 60);
+      if (h > 0) {
+        return `${h}:${m}:${s < 10 ? "0" + s : s}`;
+      } else {
+        return `${m}:${s < 10 ? "0" + s : s}`;
+      }
     }
+    this.querySelector("span").textContent = `${secondsToHms(ytPlayer.getCurrentTime())}`;
+  }
 
-    const styleClasses = ["yt-uix-button", "yt-uix-button-content", "yt-uix-button-size-default", "yt-uix-button-opacity", "no-icon-markup", "pause-resume-autoplay", "action-panel-trigger", "yt-uix-tooltip"];
+  const styleClasses = ["yt-uix-button", "yt-uix-button-content", "yt-uix-button-size-default", "yt-uix-button-opacity", "no-icon-markup", "pause-resume-autoplay", "action-panel-trigger", "yt-uix-tooltip"];
 
-    startButton.classList.add(...styleClasses);
-    stopButton.classList.add(...styleClasses);
-    sendButton.classList.add(...styleClasses);
+  startButton.classList.add(...styleClasses);
+  stopButton.classList.add(...styleClasses);
+  sendButton.classList.add(...styleClasses);
 
-    startButton.innerHTML = "Начало <span></span>";
-    stopButton.innerHTML = "Конец <span></span>";
-    sendButton.innerHTML = "Сообщить";
+  startButton.innerHTML = "Начало <span></span>";
+  stopButton.innerHTML = "Конец <span></span>";
+  sendButton.innerHTML = "Сообщить";
 
-    ytMenu.append(startButton, stopButton, sendButton);
+  ytMenu.append(startButton, stopButton, sendButton);
 
-    const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
-    console.log(`[YouTube Ads Rewind] ${videoId}`);
-    checkDB();
+  const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
+  console.log(`[YouTube Ads Rewind] ${videoId}`);
+  checkDB();
 
-    async function checkDB() {
-        let videosList = await makeRequest("GET", "https://raw.githubusercontent.com/VChet/Youtube-Ads-Rewind/master/db.json");
-        videosList = JSON.parse(videosList);
-        if (Object.keys(videosList).indexOf(videoId) > -1) {
-            console.log(`[YouTube Ads Rewind] Rewind from ${videosList[videoId].start} to ${videosList[videoId].stop}`);
-            setInterval(function () {
-                if (ytPlayer.getPlayerState() == 1) {
-                    if (ytPlayer.getCurrentTime().toFixed() == videosList[videoId].start) {
-                        console.log("[YouTube Ads Rewind] Rewinding...");
-                        ytPlayer.seekTo(videosList[videoId].stop);
-                    }
-                }
-            }, 100);
+  async function checkDB() {
+    let videosList = await makeRequest("GET", "https://raw.githubusercontent.com/VChet/Youtube-Ads-Rewind/master/db.json");
+    videosList = JSON.parse(videosList);
+    if (Object.keys(videosList).indexOf(videoId) > -1) {
+      console.log(`[YouTube Ads Rewind] Rewind from ${videosList[videoId].start} to ${videosList[videoId].stop}`);
+      setInterval(function () {
+        if (ytPlayer.getPlayerState() == 1) {
+          if (ytPlayer.getCurrentTime().toFixed() == videosList[videoId].start) {
+            console.log("[YouTube Ads Rewind] Rewinding...");
+            ytPlayer.seekTo(videosList[videoId].stop);
+          }
+        }
+      }, 100);
+    } else {
+      return;
+    }
+  }
+
+  function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+        if (this.status === 200) {
+          resolve(xhr.response);
         } else {
-            return;
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
         }
-    }
-
-    function makeRequest(method, url) {
-        return new Promise(function (resolve, reject) {
-            let xhr = new XMLHttpRequest();
-            xhr.open(method, url);
-            xhr.onload = function () {
-                if (this.status === 200) {
-                    resolve(xhr.response);
-                } else {
-                    reject({
-                        status: this.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            };
-            xhr.onerror = function () {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            };
-            xhr.send();
+      };
+      xhr.onerror = function () {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
         });
-    }
+      };
+      xhr.send();
+    });
+  }
 })();
