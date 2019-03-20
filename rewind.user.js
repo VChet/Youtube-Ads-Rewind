87dfd33a16b8ac05fc7b44e3d13b32c6a36dfb87
VChet <17050347+VChet@users.noreply.github.com>
2019-03-20 23:32:50 +0500

    Move buttons to separate function, buttons won't be added if DB is not loaded

--- rewind.user.js
+++ rewind.user.js
@@ -15,21 +15,6 @@ async function startScript() {
   const ytPlayer = document.getElementById("movie_player");
   const videoId = ytPlayer.getVideoUrl().match("(?<=v=)[^&\n?#]+")[0];
   console.log(`[YouTube Ads Rewind] Current video ID: ${videoId}`);
-  const startButton = document.createElement("button");
-  const stopButton = document.createElement("button");
-  const sendButton = document.createElement("button");
-
-  function setTiming() {
-    const secondsToHms = time => {
-      time = Number(time);
-      const h = Math.floor(time / 3600);
-      const m = Math.floor(time % 3600 / 60);
-      const s = Math.floor(time % 3600 % 60);
-      return h > 0 ? `${h}:${m}:${s < 10 ? "0" + s : s}` : `${m}:${s < 10 ? "0" + s : s}`;
-    };
-    this.querySelector("span").dataset.timecode = Math.trunc(ytPlayer.getCurrentTime());
-    this.querySelector("span").textContent = secondsToHms(ytPlayer.getCurrentTime());
-  }
 
   function makeRequest(method, url, data = {}) {
     return new Promise((resolve, reject) => {
@@ -49,46 +34,64 @@ async function startScript() {
     });
   }
 
-  async function sendTiming() {
-    const starts = startButton.querySelector("span").dataset.timecode;
-    const ends = stopButton.querySelector("span").dataset.timecode;
-    if (starts && ends) {
-      if (starts > ends) return console.warn("[YouTube Ads Rewind] Incorrect time codes");
-      const data = {
-        id: videoId,
-        timings: starts, ends
+  function addButtons() {
+    const startButton = document.createElement("button");
+    const stopButton = document.createElement("button");
+    const sendButton = document.createElement("button");
+
+    function setTiming() {
+      const secondsToHms = time => {
+        time = Number(time);
+        const h = Math.floor(time / 3600);
+        const m = Math.floor(time % 3600 / 60);
+        const s = Math.floor(time % 3600 % 60);
+        return h > 0 ? `${h}:${m}:${s < 10 ? "0" + s : s}` : `${m}:${s < 10 ? "0" + s : s}`;
       };
-      const response = await makeRequest("POST", "http://localhost:7542/api/video/report", data);
-      return console.log({ response });
-    } else {
-      return console.warn("[YouTube Ads Rewind] No time codes");
+      this.querySelector("span").dataset.timecode = Math.trunc(ytPlayer.getCurrentTime());
+      this.querySelector("span").textContent = secondsToHms(ytPlayer.getCurrentTime());
     }
+
+    async function sendTiming() {
+      const starts = startButton.querySelector("span").dataset.timecode;
+      const ends = stopButton.querySelector("span").dataset.timecode;
+      if (starts && ends) {
+        if (starts > ends) return console.warn("[YouTube Ads Rewind] Incorrect time codes");
+        const data = {
+          id: videoId,
+          timings: starts, ends
+        };
+        const response = await makeRequest("POST", "http://localhost:7542/api/video/report", data);
+        return console.log({ response });
+      } else {
+        return console.warn("[YouTube Ads Rewind] No time codes");
+      }
+    }
+
+    startButton.addEventListener("click", setTiming);
+    stopButton.addEventListener("click", setTiming);
+    sendButton.addEventListener("click", sendTiming);
+
+    const styleClasses = [
+      "action-panel-trigger",
+      "no-icon-markup",
+      "pause-resume-autoplay",
+      "yt-uix-button-content",
+      "yt-uix-button-opacity",
+      "yt-uix-button-size-default",
+      "yt-uix-button",
+      "yt-uix-tooltip"
+    ];
+
+    startButton.classList.add(...styleClasses);
+    stopButton.classList.add(...styleClasses);
+    sendButton.classList.add(...styleClasses);
+
+    startButton.innerHTML = "Начало <span></span>";
+    stopButton.innerHTML = "Конец <span></span>";
+    sendButton.innerHTML = "Сообщить";
+
+    ytMenu.append(startButton, stopButton, sendButton);
   }
-
-  startButton.addEventListener("click", setTiming);
-  stopButton.addEventListener("click", setTiming);
-  sendButton.addEventListener("click", sendTiming);
-
-  const styleClasses = [
-    "action-panel-trigger",
-    "no-icon-markup",
-    "pause-resume-autoplay",
-    "yt-uix-button-content",
-    "yt-uix-button-opacity",
-    "yt-uix-button-size-default",
-    "yt-uix-button",
-    "yt-uix-tooltip"
-  ];
-
-  startButton.classList.add(...styleClasses);
-  stopButton.classList.add(...styleClasses);
-  sendButton.classList.add(...styleClasses);
-
-  startButton.innerHTML = "Начало <span></span>";
-  stopButton.innerHTML = "Конец <span></span>";
-  sendButton.innerHTML = "Сообщить";
-
-  ytMenu.append(startButton, stopButton, sendButton);
 
   let videoTimer;
   function rewind(videoData) {
@@ -110,6 +113,7 @@ async function startScript() {
   const videosList = await makeRequest("GET", "http://localhost:7542/api/videos");
   if (!videosList) return console.log("[YouTube Ads Rewind] Error loading DB");
   console.log("[YouTube Ads Rewind] DB has been loaded", videosList);
+  addButtons();
   const videoData = videosList.find(el => el.videoId === videoId);
   if (videoData) {
     videoData.timings.map(timing => console.log(`[YouTube Ads Rewind] Rewind from ${timing.starts} to ${timing.ends}`));
